8018c0b3a3ea8217675d5fc6c809b3bac9a8538a
james macharia <wanyakiago@gmail.com>
2018-07-13 18:31:31 +0300

    addinng js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// alert('hallo habiba');+-
+
+
+$(document).ready(function(){
+  $("form#favourite").submit(function(event){
+    event.preventDefault();
+    var userInput = parseInt($("input#favNumber").val());
+    console.log(userInput);
+
+ for (var i=1; i<=userInput; i++){
+  var result="";
+     if (i % 3 !=0 && i % 5 !=0) result=(i);
+  if( i % 3 === 0) result="ping";
+     if(i % 5 === 0) result +="pong";
+   console.log(result);
