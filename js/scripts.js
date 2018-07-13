e6589b2e05c5d59038c20e8c2888bff675e20a87
james macharia <wanyakiago@gmail.com>
2018-07-13 20:33:35 +0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ jQuery(document).ready(function(){
     event.preventDefault();
     var userInput = parseInt($("input#favNumber").val());
     console.log(userInput);
+    $(".display").empty(); // clear the initial input if present
 
- for (var i=1; i<=userInput; i++){
   var result="";
      if (i % 3 !=0 && i % 5 !=0) result=(i);
   if( i % 3 === 0) result="ping";
